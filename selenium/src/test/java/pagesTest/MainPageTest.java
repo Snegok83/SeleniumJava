44b75a15f26829dd38f74218b6592d49c6fde9b4
Snegok83 <38438492+Snegok83@users.noreply.github.com>
2018-04-17 09:59:02 -0500

    project created

---
+++ selenium/src/test/java/pagesTest/MainPageTest.java
@@ -0,0 +1,19 @@
+package pagesTest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class MainPageTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
