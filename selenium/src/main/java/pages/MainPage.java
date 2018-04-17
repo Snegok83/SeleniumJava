f5eed1673b2f18051a98e1950ac049aa7d2cc211
Snegok83 <38438492+Snegok83@users.noreply.github.com>
2018-04-17 11:57:50 -0500

    updated classpath, MainPage.java, MainPageTest.java

--- selenium/src/main/java/pages/MainPage.java
+++ selenium/src/main/java/pages/MainPage.java
@@ -1,5 +1,37 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainPage {
 
+	@FindBy(id = "search_query_top")
+	WebElement searchTop;
+	@FindBy(name="submit_search")
+	WebElement btnSubmit;
+	
+	WebDriver driver;
+
+	public MainPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void checkSearch(String searchWord) {
+
+		searchTop.sendKeys(searchWord);
+	}
+
+	public void clickSearchBtn() {
+		btnSubmit.click();
+	}
+	
+	public void clickBack() {
+		
+		driver.navigate().back();
+		
+	}
 }
--- selenium/src/test/java/pagesTest/MainPageTest.java
+++ selenium/src/test/java/pagesTest/MainPageTest.java
@@ -2,18 +2,46 @@ package pagesTest;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
+import pages.MainPage;
 
 public class MainPageTest {
-  @Test
-  public void f() {
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
 
-  @AfterTest
-  public void afterTest() {
-  }
+	private static String driverType = "webdriver.chrome.driver";
+	private static String pathToDriver = "C:\\tmp\\chromedriver.exe";
+	WebDriver driver;
+
+	private String searchWord = "test";
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty(driverType, pathToDriver);
+
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	@Test
+	public void checkSearch() {
+		MainPage mp = PageFactory.initElements(driver, MainPage.class);
+		mp.checkSearch(searchWord);
+		mp.clickSearchBtn();
+		mp.clickBack();
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		//driver.close();
+	}
 
 }
